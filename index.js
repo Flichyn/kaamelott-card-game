bb64cc140014da8ef4cb0339efc1c7d8fa870c93
chenuetLoic <loicchenuet@hotmail.fr>
2022-03-08 11:57:30 +0100

    mise en place du plateau

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+const MAX_LIFE = 30;
+const MANA = 0;
+
 class Player {
+    constructor(player, life, mana) {
+        this.player = player;
+        this.life = MAX_LIFE;
+        this.mana = MANA;
+    }
 
 }
+
+const startGame = document.querySelector('#start-game');
+startGame.style.backgroundImage = "url('background.webp')";
+startGame.style.minWidth = "100%";
+startGame.style.minHeight = "100%";
+startGame.style.position = "absolute";
+startGame.style.display = "flex";
+startGame.style.justifyContent = "center";
+startGame.style.alignItems = "center";
+
+const gameBoard = document.querySelector('#game-board');
+const buttonNone = document.querySelector('#button-none');
+buttonNone.addEventListener('click', function () {
+    startGame.style.display = "none";
+    gameBoard.style.display = "block";
+})
+
